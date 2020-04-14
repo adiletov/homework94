153c6ec581d59dafd3b2fdb947cd3940e4bea0a7
adiladiletov <nurlubek009>
2020-04-14 14:04:17 +0600

    измеÐchanged the paths to pictures in fixtures

--- backend/fixtures.js
+++ backend/fixtures.js
@@ -20,26 +20,26 @@ const run = async () => {
             fullName: 'John doe',
             username: 'john',
             password: 'john123',
-            avatar: 'fixtures/john_doe.jpeg',
+            avatar: 'john_doe.jpeg',
             token: nanoid(),
         }, {
             fullName: 'Jason Stathom',
             username: 'jason',
             password: 'jason123',
             token: nanoid(),
-            avatar: 'fixtures/jason.jpeg'
+            avatar: 'jason.jpeg'
         }, {
             fullName: 'Tom Hardy',
             username: 'hardy',
             password: 'hardy123',
             token: nanoid(),
-            avatar: 'fixtures/hardy.jpeg'
+            avatar: 'hardy.jpeg'
         }, {
             fullName: 'Usain Bolt',
             username: 'usain',
             password: 'usain123',
             token: nanoid(),
-            avatar: 'fixtures/usain.jpeg'
+            avatar: 'usain.jpeg'
         }
     );
 
@@ -49,28 +49,28 @@ const run = async () => {
             description: '',
             userId: users[0]._id,
             tags: ['Serj Tankian', 'Daron Malakian', 'John Dolmayan', 'Shavo Odadjian and me'],
-            image: 'fixtures/jondPubl.jpeg',
+            image: 'jondPubl.jpeg',
             datetime: '4/09/2020, 19:09:45 PM'
         }, {
             title: 'Hello this is very good day!',
             description: '',
             userId: users[0]._id,
             tags: ['good', 'live', 'like'],
-            image: 'fixtures/goodday.jpg',
+            image: 'goodday.jpg',
             datetime: '3/11/2020, 3:09:45 PM'
         }, {
             title: 'Back at it with @guyritchie Bring your chess clock and your wallet. @danielsmithphotography!',
             description: '',
             userId: users[1]._id,
             tags: ['cheese'],
-            image: 'fixtures/cheese.jpg',
+            image: 'cheese.jpg',
             datetime: '3/10/2020, 6:09:45 PM'
         }, {
             title: 'Thermal race track. @charlietadman @markgillespie @mclarenauto',
             description: '',
             userId: users[1]._id,
             tags: ['race', 'car', 'formula'],
-            image: 'fixtures/race.jpg',
+            image: 'race.jpg',
             datetime: '4/12/2020, 6:09:45 PM'
         },
         {
@@ -78,7 +78,7 @@ const run = async () => {
             description: '',
             userId: users[2]._id,
             tags: ['box', 'friends'],
-            image: 'fixtures/tom.jpg',
+            image: 'tom.jpg',
             datetime: '4/13/2020, 5:09:45 PM'
         },
         {
@@ -86,7 +86,7 @@ const run = async () => {
             description: 'Подтвержденный Well Done Vikki and Dutch ',
             userId: users[2]._id,
             tags: ['@bikeshedmc'],
-            image: 'fixtures/tom2.jpeg',
+            image: 'tom2.jpeg',
             datetime: '4/12/2020, 3:09:45 PM'
         },
         {
@@ -94,7 +94,7 @@ const run = async () => {
             description: ' ',
             userId: users[3]._id,
             tags: ['HappyEaster'],
-            image: 'fixtures/usain2.jpg',
+            image: 'usain2.jpg',
             datetime: '4/13/2020, 7:09:45 PM'
         },
         {
@@ -102,7 +102,7 @@ const run = async () => {
             description: ' ',
             userId: users[3]._id,
             tags: ['ateamlifestyle', 'briscobal', '_jayenigma'],
-            image: 'fixtures/usain3.jpg',
+            image: 'usain3.jpg',
             datetime: '4/13/2020, 2:09:45 PM'
         },
     );
